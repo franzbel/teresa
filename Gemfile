2f029908cae5ac66dafa4b462bd99b549e045001
Franz Beltran <franzbeltran79@gmail.com>
2015-12-16 11:27:29 -0400

    Agregar usuarios con faker

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'faker', '~> 1.6', '>= 1.6.1'
 gem 'bcrypt', '~> 3.1', '>= 3.1.10'
 gem 'paperclip', '~> 4.3'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do
+  User.create!(name: Faker::Name.first_name,
+               email: Faker::Internet.free_email,
+               password: '12345678',
+               password_confirmation: '12345678')
+end
